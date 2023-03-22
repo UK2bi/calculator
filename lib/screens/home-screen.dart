7680522462c7c7ec01abb106cc2194585ed93f66
UK2bi <ukmainor@gmail.com>
2023-03-22 21:49:48 +0200

    Initial commit

--- lib/screens/home-screen.dart
+++ lib/screens/home-screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../controller/logic.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.title});
 
@@ -10,64 +12,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String output = "0";
 
-  String _output = "0";
-  double num1 = 0.0;
-  double num2 = 0.0;
-  String operand = "";
-
-  buttonPressed(String buttonText) {
-    if (buttonText == "CLEAR") {
-      _output = "0";
-      num1 = 0.0;
-      num2 = 0.0;
-      operand = "";
-    } else if (buttonText == "+" ||
-        buttonText == "-" ||
-        buttonText == "/" ||
-        buttonText == "X") {
-      num1 = double.parse(output);
-
-      operand = buttonText;
-
-      _output = "0";
-    } else if (buttonText == ".") {
-      if (_output.contains(".")) {
-        print("Already conatains a decimals");
-        return;
-      } else {
-        _output = _output + buttonText;
-      }
-    } else if (buttonText == "=") {
-      num2 = double.parse(output);
-
-      if (operand == "+") {
-        _output = (num1 + num2).toString();
-      }
-      if (operand == "-") {
-        _output = (num1 - num2).toString();
-      }
-      if (operand == "X") {
-        _output = (num1 * num2).toString();
-      }
-      if (operand == "/") {
-        _output = (num1 / num2).toString();
-      }
-
-      num1 = 0.0;
-      num2 = 0.0;
-      operand = "";
-    } else {
-      _output = _output + buttonText;
-    }
-
-    print(_output);
-
-    setState(() {
-      output = double.parse(_output).toStringAsFixed(2);
-    });
-  }
 
   Widget buildButton(String buttonText) {
     return Expanded(
@@ -79,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
             style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
           ),
         ),
-        onPressed: () => buttonPressed(buttonText),
+        onPressed: buttonPressed(buttonText,
       ),
     );
   }
@@ -92,44 +37,44 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         body: Container(
             child: Column(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.centerRight,
-                padding: EdgeInsets.symmetric(vertical: 24, horizontal: 12),
-                child: Text(output,
-                    style: TextStyle(
-                        fontSize: 48.0, fontWeight: FontWeight.bold))),
-            Expanded(
-              child: Divider(),
-            ),
-            Column(children: [
-              Row(children: [
-                buildButton("7"),
-                buildButton("8"),
-                buildButton("9"),
-                buildButton("/")
-              ]),
-              Row(children: [
-                buildButton("4"),
-                buildButton("5"),
-                buildButton("6"),
-                buildButton("X")
-              ]),
-              Row(children: [
-                buildButton("1"),
-                buildButton("2"),
-                buildButton("3"),
-                buildButton("-")
-              ]),
-              Row(children: [
-                buildButton("."),
-                buildButton("0"),
-                buildButton("00"),
-                buildButton("+")
-              ]),
-              Row(children: [buildButton("CLEAR"), buildButton("=")]),
-            ])
-          ],
-        )));
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.symmetric(vertical: 24, horizontal: 12),
+                    child: Text(output,
+                        style: TextStyle(
+                            fontSize: 48.0, fontWeight: FontWeight.bold))),
+                Expanded(
+                  child: Divider(),
+                ),
+                Column(children: [
+                  Row(children: [
+                    buildButton("7"),
+                    buildButton("8"),
+                    buildButton("9"),
+                    buildButton("/")
+                  ]),
+                  Row(children: [
+                    buildButton("4"),
+                    buildButton("5"),
+                    buildButton("6"),
+                    buildButton("X")
+                  ]),
+                  Row(children: [
+                    buildButton("1"),
+                    buildButton("2"),
+                    buildButton("3"),
+                    buildButton("-")
+                  ]),
+                  Row(children: [
+                    buildButton("."),
+                    buildButton("0"),
+                    buildButton("00"),
+                    buildButton("+")
+                  ]),
+                  Row(children: [buildButton("CLEAR"), buildButton("=")]),
+                ])
+              ],
+            )));
   }
 }
