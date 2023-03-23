c79fc1593d3fbd536bc9537ba08176f041aa96b2
UK2bi <ukmainor@gmail.com>
2023-03-23 15:45:27 +0200

    Initial commit 2

--- lib/screens/home-screen.dart
+++ lib/screens/home-screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../controller/logic.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.title});
@@ -12,7 +11,76 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String output = "0";
 
+  String _output = "0";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String operand = "";
+
+  buttonPressed(String buttonText){
+
+    if(buttonText == "CLEAR"){
+
+      _output = "0";
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+
+    } else if (buttonText == "+" || buttonText == "-" || buttonText == "/" || buttonText == "X"){
+
+      num1 = double.parse(output);
+
+      operand = buttonText;
+
+      _output = "0";
+
+    } else if(buttonText == "."){
+
+      if(_output.contains(".")){
+        print("Already conatains a decimals");
+        return;
+
+      } else {
+        _output = _output + buttonText;
+      }
+
+    } else if (buttonText == "="){
+
+      num2 = double.parse(output);
+
+      if(operand == "+"){
+        _output = (num1 + num2).toString();
+      }
+      if(operand == "-"){
+        _output = (num1 - num2).toString();
+      }
+      if(operand == "X"){
+        _output = (num1 * num2).toString();
+      }
+      if(operand == "/"){
+        _output = (num1 / num2).toString();
+      }
+
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+
+    } else {
+
+      _output = _output + buttonText;
+
+    }
+
+    print(_output);
+
+    setState(() {
+
+      output = double.parse(_output).toStringAsFixed(2);
+
+    });
+
+  }
 
   Widget buildButton(String buttonText) {
     return Expanded(
@@ -24,7 +92,9 @@ class _HomeScreenState extends State<HomeScreen> {
             style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
           ),
         ),
-        onPressed: buttonPressed(buttonText,
+        onPressed: () =>
+            buttonPressed(buttonText)
+        ,
       ),
     );
   }
